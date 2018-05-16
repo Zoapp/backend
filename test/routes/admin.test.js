77278be23515f87fc0ce8776102a10b52cb14942
Manuel Raynaud <manu@raynaud.io>
2018-05-16 15:42:32 +0200

    getParameters route will throw an error if there is no result

---
+++ test/routes/admin.test.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import chai from "chai";
+import ApiError from "zoauth-server/Error/ApiError";
+import Admin from "../../src/routes/admin";
+
+const { expect } = chai;
+
+describe("admin", () => {
+  describe("getParameterValue", () => {
+    it("return value found in database", async () => {
+      const context = {
+        getParams: () => ({ name: "foo" }),
+      };
+
+      const parameters = {
+        getValue: async (name) => name,
+      };
+
+      const controller = {
+        getParameters: () => parameters,
+      };
+
+      const admin = new Admin(controller);
+      const result = await admin.getParameterValue(context);
+
+      expect(result).to.equal("foo");
+    });
+
+    it("throws an ApiError if nothing is found", async () => {
+      const context = {
+        getParams: () => ({ name: "foo" }),
+      };
+
+      const parameters = {
+        getValue: async () => null,
+      };
+
+      const controller = {
+        getParameters: () => parameters,
+      };
+
+      const admin = new Admin(controller);
+      try {
+        await admin.getParameterValue(context);
+        throw new Error("an error should be return before");
+      } catch (error) {
+        expect(error).to.be.an.instanceof(ApiError);
+        expect(error.status).to.be.equal(404);
+      }
+    });
+  });
+});
