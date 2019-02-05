356919f6c8e5114c65776f2606d49a12661401fb
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2019-02-05 12:38:23 +0100

    Update zoauth-server to 0.10.8

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import ApiError from "zoauth-server/errors/ApiError";
+import { ApiError } from "zoauth-server/errors";
 import CommonRoutes from "./common";
 import deleteUndefined from "../utils/utilsObject";
 
